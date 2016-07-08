5b0dbe1965b0eb8c2cd4f669c045a203b0f3b7d4
Juha Törnroos <juha.tornroos@csc.fi>
2016-07-08 09:48:01 -0400

    TPAS-16 Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+def scripts_list():
+    """Return list of command line tools from package pas.scripts"""
+    scripts = []
+    for modulename in os.listdir('siptools/scripts'):
+        if modulename == '__init__.py':
+            continue
+        if not modulename.endswith('.py'):
+            continue
+        modulename = modulename.replace('.py','')
+        scriptname = modulename.replace('_','-')
+        scripts.append('%s = ipt.scripts.%s:main' % (scriptname, modulename))
+    print scripts
+    return scripts
+
+def main():
+    """Install siptools"""
+    setup(
+        name='siptools',
+	packages=find_packages(exclude=['tests', 'tests.*']),
+        version='dev',
+        entry_points = {'console_scripts': scripts_list()})
+
+if __name__ == '__main__':
+    main()
