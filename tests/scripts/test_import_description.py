98d456529a8d76077f19cc2d493e4e5ee70fc4e3
Kuisma Lehtonen <kuisma.lehtonen@csc.fi>
2016-10-22 18:46:54 +0300

    pep-8 modifications

--- tests/scripts/test_import_description.py
+++ tests/scripts/test_import_description.py
@@ -12,6 +12,7 @@ from urllib import quote
 # loydy, tiedosto ei ole xml:aa, metadata ei ole luettelossa (vaara
 # nimiavaruus
 
+
 def validate_dmd_files(workspace, dmdsec_location):
     """ Validate created xml-file by parser"""
 
@@ -19,25 +20,26 @@ def validate_dmd_files(workspace, dmdsec_location):
     workspace_path = os.path.abspath(workspace)
     if os.path.isdir(dmd_path):
         for root, dirs, files in os.walk(dmd_path, topdown=False):
-             for name in files:
-                 url_t_path = quote(dmdsec_location,safe='') + name
-                 t_path = os.path.join(workspace, url_t_path)
-                 with open(t_path, 'r') as content_file:
-                     content = content_file.read()
-                     parser = lxml.etree.XMLParser(
-                             dtd_validation=False, no_network=True)
-                     tree = lxml.etree.fromstring(content)
+            for name in files:
+                url_t_path = quote(dmdsec_location, safe='') + name
+                t_path = os.path.join(workspace, url_t_path)
+                with open(t_path, 'r') as content_file:
+                    content = content_file.read()
+                    parser = lxml.etree.XMLParser(
+                        dtd_validation=False, no_network=True)
+                    tree = lxml.etree.fromstring(content)
     else:
         if not os.path.isfile(dmd_path):
-            raise IOError( "File or directory not found: %s" % dmd_path )
+            raise IOError("File or directory not found: %s" % dmd_path)
         filename = os.path.basename(dmd_path)
-        url_t_path = quote(dmdsec_location,safe='')
+        url_t_path = quote(dmdsec_location, safe='')
         t_path = os.path.join(workspace, url_t_path)
         with open(t_path, 'r') as content_file:
             content = content_file.read()
             parser = lxml.etree.XMLParser(
-                    dtd_validation=False, no_network=True)
+                dtd_validation=False, no_network=True)
             tree = lxml.etree.fromstring(content)
+
 
 def test_import_description_valid_file():
     """ Test case for single valid xml-file"""
@@ -47,11 +49,13 @@ def test_import_description_valid_file():
     main([dmdsec_location,  '--workspace', workspace])
     validate_dmd_files(workspace, dmdsec_location)
 
+
 def test_import_description_no_workspace():
     """ Test case for single valid xml-file. Uses default workspace location."""
     dmdsec_location = 'tests/data/import_description/metadata/dc_description.xml'
     main([dmdsec_location])
     validate_dmd_files("./", dmdsec_location)
+
 
 def test_import_description_valid_directory():
     """ Test case for metadata directory, which consists of several valid
@@ -61,6 +65,7 @@ def test_import_description_valid_directory():
     main([dmdsec_location,  '--workspace', workspace])
     validate_dmd_files(workspace, dmdsec_location)
 
+
 def test_import_description_file_not_found():
     """ Test case for not existing xml-file."""
     dmdsec_location = 'tests/data/import_description/metadata/dc_description_not_found.xml'
@@ -68,12 +73,14 @@ def test_import_description_file_not_found():
     with pytest.raises(IOError):
         main([dmdsec_location,  '--workspace', workspace])
 
+
 def test_import_description_no_xml():
     """ test case for invalid XML file """
     dmdsec_location = 'tests/data/import_description/metadata/plain_text.xml'
     workspace = './workspace/mets-parts'
     main([dmdsec_location,  '--workspace', workspace])
     validate_dmd_files(workspace, dmdsec_location)
+
 
 def test_import_description_invalid_namespace():
     """ test case for invalid namespace in XML file """
--- tests/scripts/test_premis_event.py
+++ tests/scripts/test_premis_event.py
@@ -4,36 +4,45 @@ from siptools.scripts import premis_event
 import pytest
 import os
 
+
 def test_premis_event_ok():
 
-    event_type ='creation'
+    event_type = 'creation'
 
     return_code = premis_event.main([event_type, '2016-10-13T12:30:55',
-        '--event_detail', 'Testing', '--event_outcome', 'success',
-        '--event_outcome_detail', 'Outcome detail', '--workspace',
-        './workspace', '--agent_name', 'Demo Application', '--agent_type', 'software'])
+                                     '--event_detail', 'Testing', '--event_outcome', 'success',
+                                     '--event_outcome_detail', 'Outcome detail', '--workspace',
+                                     './workspace', '--agent_name', 'Demo Application', '--agent_type', 'software'])
 
     output_file = os.path.join('./workspace', event_type + '.xml')
     tree = ET.parse(output_file)
     root = tree.getroot()
-    #print "root: %s" % ET.tostring(root, encoding='UTF-8', method='xml')
+    # print "root: %s" % ET.tostring(root, encoding='UTF-8', method='xml')
 
     assert len(root.findall('{http://www.loc.gov/METS/}amdSec')) == 1
-    assert root.findall(".//{info:lc/xmlns/premis-v2}eventType")[0].text == 'creation'
-    assert root.findall(".//{info:lc/xmlns/premis-v2}eventDateTime")[0].text == '2016-10-13T12:30:55'
-    assert root.findall(".//{info:lc/xmlns/premis-v2}eventDetail")[0].text == 'Testing'
-    assert root.findall(".//{info:lc/xmlns/premis-v2}eventOutcome")[0].text == 'success'
-    assert root.findall(".//{info:lc/xmlns/premis-v2}eventOutcomeDetailNote")[0].text == 'Outcome detail'
-    assert root.findall(".//{info:lc/xmlns/premis-v2}agentName")[0].text == 'Demo Application'
-    assert root.findall(".//{info:lc/xmlns/premis-v2}agentType")[0].text == 'software'
+    assert root.findall(
+        ".//{info:lc/xmlns/premis-v2}eventType")[0].text == 'creation'
+    assert root.findall(
+        ".//{info:lc/xmlns/premis-v2}eventDateTime")[0].text == '2016-10-13T12:30:55'
+    assert root.findall(
+        ".//{info:lc/xmlns/premis-v2}eventDetail")[0].text == 'Testing'
+    assert root.findall(
+        ".//{info:lc/xmlns/premis-v2}eventOutcome")[0].text == 'success'
+    assert root.findall(
+        ".//{info:lc/xmlns/premis-v2}eventOutcomeDetailNote")[0].text == 'Outcome detail'
+    assert root.findall(
+        ".//{info:lc/xmlns/premis-v2}agentName")[0].text == 'Demo Application'
+    assert root.findall(
+        ".//{info:lc/xmlns/premis-v2}agentType")[0].text == 'software'
 
     assert return_code == 0
 
+
 def test_premis_event_fail():
 
-    event_type ='nonsense'
+    event_type = 'nonsense'
 
     with pytest.raises(SystemExit):
         return_code = premis_event.main([event_type, '2016-10-13T12:30:55',
-            '--event_detail', 'Testing', '--event_outcome', 'success',
-            '--event_outcome_detail', 'Outcome detail', '--workspace', './workspace'])
+                                         '--event_detail', 'Testing', '--event_outcome', 'success',
+                                         '--event_outcome_detail', 'Outcome detail', '--workspace', './workspace'])
