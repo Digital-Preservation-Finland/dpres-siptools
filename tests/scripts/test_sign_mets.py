c23d388f2883c7b6adbedd0bdc86530a2656dbcd
Juha Törnroos <juha.tornroos@csc.fi>
2016-10-26 22:40:17 -0400

    TPAS-12 Implement a sign-mets tool for digitally signing mets xml file

---
+++ tests/scripts/test_sign_mets.py
@@ -0,0 +1,15 @@
+from tempfile import NamedTemporaryFile 
+import siptools.scripts.sign_mets
+import pytest
+
+def test_valid_sign_mets():
+
+
+    output=NamedTemporaryFile().name
+    signing_key='tests/data/rsa-keys.crt'
+    arguments=['tests/data/text-file.txt', output, signing_key]
+
+    assert siptools.scripts.sign_mets.main(arguments) == 0
+
+    with open(output) as fd:
+        assert "4ddd69b778405b4072d77762a85f9cf5e8e5ca83" in fd.read()
