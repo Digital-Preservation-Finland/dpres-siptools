f552c32aa2062a84887de5608d33b00d033dfd0f
Kuisma Lehtonen <kuisma.lehtonen@csc.fi>
2017-01-11 02:41:05 +0200

    TPAS-53 Value 'dc' is not allowed in attribute 'MDTYPE' in element 'ns0:mdWrap'.  Valid values are: MARC; DC; MODS; EAD; EAC-CPF; LIDO; VRA; DDI; OTHER

--- siptools/xml/namespaces.py
+++ siptools/xml/namespaces.py
@@ -9,18 +9,18 @@ NAMESPACES = {
     'mix': 'http://www.loc.gov/mix/v20'
 }
 
-METS_NS = {'http://purl.org/dc/elements/1.1/' : {'mdtype' : 'dc', 'version' :
-''}, 'http://www.loc.gov/MARC21/slim' : {'mdtype' : 'marc', 'version' : ''},
-'http://www.loc.gov/mods/v3' : {'mdtype' : 'mods', 'version' : ''},
-'urn:isbn:1-931666-22-9': {'mdtype' : 'ead', 'version' : ''},
-'http://ead3.archivists.org/schema/': {'mdtype' : 'ead3', 'version' : ''},
-'urn:isbn:1-931666-33-4' : {'mdtype' : 'eac', 'version' : ''},
-'http://www.lido-schema.org' : {'mdtype' : 'lido', 'version' : ''},
-'ddi:instance:3_2' : {'mdtype' : 'ddi', 'version' : '3.2'},
-'ddi:instance:3_1' : {'mdtype' : 'ddi', 'version' : '3.1'},
-'ddi:codebook:2_5' : {'mdtype' : 'ddi', 'version' : '2.5'},
-'http://www.icpsr.umich.edu/DDI' : {'mdtype' : 'ddi', 'version' : '2.1'},
-'http://www.vraweb.org/vracore4.htm' : {'mdtype' : 'vra', 'version' : ''}}
+METS_NS = {'http://purl.org/dc/elements/1.1/' : {'mdtype' : 'DC', 'version' :
+''}, 'http://www.loc.gov/MARC21/slim' : {'mdtype' : 'MARC', 'version' : ''},
+'http://www.loc.gov/mods/v3' : {'mdtype' : 'MODS', 'version' : ''},
+'urn:isbn:1-931666-22-9': {'mdtype' : 'EAD', 'version' : ''},
+'http://ead3.archivists.org/schema/': {'mdtype' : 'EAD3', 'version' : ''},
+'urn:isbn:1-931666-33-4' : {'mdtype' : 'EAC', 'version' : ''},
+'http://www.lido-schema.org' : {'mdtype' : 'LIDO', 'version' : ''},
+'ddi:instance:3_2' : {'mdtype' : 'DDI', 'version' : '3.2'},
+'ddi:instance:3_1' : {'mdtype' : 'DDI', 'version' : '3.1'},
+'ddi:codebook:2_5' : {'mdtype' : 'DDI', 'version' : '2.5'},
+'http://www.icpsr.umich.edu/DDI' : {'mdtype' : 'DDI', 'version' : '2.1'},
+'http://www.vraweb.org/vracore4.htm' : {'mdtype' : 'VRA', 'version' : ''}}
 
 METS_PROFILE = {'kdk': 'http://www.kdk.fi/kdk-mets-profile'}
 
