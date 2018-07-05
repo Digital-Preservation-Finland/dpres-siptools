82ab23ae687b3ab80d3c29571ef87c1e77c8ac74
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-07-05 12:49:22 +0300

    TPAS-89 PEP8 corrections and documentation

--- siptools/scripts/create_mix.py
+++ siptools/scripts/create_mix.py
@@ -1,69 +1,77 @@
 # coding=utf-8
-""" Command line tool for creating mix
-"""
+"""Command line tool for creating mix metadata."""
 
 import os
-import sys
-import wand.image
-from PIL import Image
 import argparse
-import nisomix.mix as mix
-import mets as m
-import xml_helpers.utils as h
+import wand.image
+import PIL.Image
+import nisomix.mix
+import mets
+import xml_helpers.utils
 from siptools.utils import encode_path, encode_id
 
 
 def parse_arguments(arguments):
-    """Parse arguments
-    """
+    """Parse arguments commandline arguments."""
     parser = argparse.ArgumentParser(
-        description="Tool for creating mix metadata for an image")
+        description="Tool for creating mix metadata for an image. The MIX "
+                    "metadata is written to "
+                    "MIX<url-encoded-file-path>-othermd.xml -file in the "
+                    "workspace directory."
+    )
     parser.add_argument('filename', type=str,
-        help="Image file to be described as mix metadata")
-    parser.add_argument(
-        '--workspace', type=str, default='./workspace/',
-        help="Workspace directory for the metadata files.")
+                        help="Image file to be described as mix metadata")
+    parser.add_argument('--workspace', type=str, default='./workspace/',
+                        help="Workspace directory for the metadata files.")
 
     return parser.parse_args(arguments)
 
 
 def main(arguments=None):
-    """The main method for argparser"""
+    """Write MIX metadata for a image file."""
     args = parse_arguments(arguments)
 
-    #mddata = write_mix(os.path.join(args.workspace, args.filename))
     filename = encode_path(args.filename, prefix='MIX', suffix="-othermd.xml")
     fileid = encode_id(filename)
-    mets = create_mix_techmd(args.filename, fileid)
+    mets_ = create_mix_techmd(args.filename, fileid)
 
     with open(os.path.join(args.workspace, filename), 'w+') as outfile:
-        outfile.write(h.serialize(mets))
+        outfile.write(xml_helpers.utils.serialize(mets_))
     print "Wrote METS MIX technical metadata to file %s" % outfile.name
 
 
 def create_mix_techmd(filename, fileid=None):
-    mddata = write_mix(os.path.join(filename))
+    """Creates METS XML element that contains techMD element with MIX metadata.
+
+    :filename: Filename of MIX metadata file
+    :fileid: ID of MIX metadata file
+    :returns: METS XML element
+    """
+    mddata = create_mix(os.path.join(filename))
     if fileid is None:
-      filename = encode_path(filename, prefix='MIX', suffix="-othermd.xml")
-      fileid = encode_id(filename)
-    mets = m.mets()
-    amdsec = m.amdsec()
-    techmd = m.techmd(fileid)
-    mdwrap = m.mdwrap('NISOIMG', "2.0")
-    xmldata = m.xmldata()
+        filename = encode_path(filename, prefix='MIX', suffix="-othermd.xml")
+        fileid = encode_id(filename)
+    mets_ = mets.mets()
+    amdsec = mets.amdsec()
+    techmd = mets.techmd(fileid)
+    mdwrap = mets.mdwrap('NISOIMG', "2.0")
+    xmldata = mets.xmldata()
 
     xmldata.append(mddata)
     mdwrap.append(xmldata)
     techmd.append(mdwrap)
     amdsec.append(techmd)
-    mets.append(amdsec)
+    mets_.append(amdsec)
 
-    return mets
+    return mets_
 
 
-def write_mix(img):
-    """Write MIX technical metadata if it's an image file. Use both
+def create_mix(img):
+    """Create MIX technical metadata XML element for an image file. Use both
     Wand and Pillow modules to extract metadata from image file.
+
+    :img: image file path
+    :returns: MIX XML element
     """
     with wand.image.Image(filename=img) as i:
         byteorder = None
@@ -80,7 +88,7 @@ def write_mix(img):
                 elif value == 'lsb':
                     byteorder = 'little endian'
 
-    with Image.open(img) as image:
+    with PIL.Image.open(img) as image:
         mode = image.mode
         if mode == 'F':
             bpsunit = 'floating point'
@@ -110,21 +118,25 @@ def write_mix(img):
                 if key == mode:
                     samplesperpixel = value
 
-    mix_compression = mix.mix_Compression(compressionScheme=compression)
+    mix_compression \
+        = nisomix.mix.mix_Compression(compressionScheme=compression)
 
-    basicdigitalobjectinformation = mix.mix_BasicDigitalObjectInformation(
-        byteOrder=byteorder, Compression_elements=[mix_compression])
+    basicdigitalobjectinformation \
+        = nisomix.mix.mix_BasicDigitalObjectInformation(
+            byteOrder=byteorder,
+            Compression_elements=[mix_compression]
+        )
 
-    basicimageinformation = mix.mix_BasicImageInformation(
+    basicimageinformation = nisomix.mix.mix_BasicImageInformation(
         imageWidth=width,
         imageHeight=height,
         colorSpace=colorspace)
 
-    imageassessmentmetadata = mix.mix_ImageAssessmentMetadata(
+    imageassessmentmetadata = nisomix.mix.mix_ImageAssessmentMetadata(
         bitsPerSampleValue_elements=bitspersample,
         bitsPerSampleUnit=bpsunit, samplesPerPixel=samplesperpixel)
 
-    mix_root = mix.mix_mix(
+    mix_root = nisomix.mix.mix_mix(
         BasicDigitalObjectInformation=basicdigitalobjectinformation,
         BasicImageInformation=basicimageinformation,
         ImageAssessmentMetadata=imageassessmentmetadata)
@@ -133,6 +145,4 @@ def write_mix(img):
 
 
 if __name__ == '__main__':
-    RETVAL = main()
-    sys.exit(RETVAL)
-
+    main()
