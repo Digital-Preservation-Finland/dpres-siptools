42487e6bf2f3e54df1e207e8ce695457709bb6dd
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-07-10 16:11:28 +0300

    TPAS-89 Add test for create_techmdfile function

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,6 +1,8 @@
 """Tests for the utility functions."""
 
-from siptools.utils import encode_path, decode_path
+import os
+import lxml.etree
+from siptools.utils import encode_path, decode_path, create_techmdfile
 
 
 def test_encode_path():
@@ -29,3 +31,31 @@ def test_decode_path():
 
     decoded_path = decode_path('t%C3%A4sts%2Ft%C3%B8stpath')
     assert decoded_path == u't\u00e4sts/t\u00f8stpath'
+
+
+def test_create_techmdfile(testpath):
+    """Test create_techmdfile function. Pass a dummy XML element to the
+    function and check that XML file with correct filename is created to
+    workspace. Check that XML file contains expected elements.
+    """
+
+    sample_data = lxml.etree.Element('sampleData')
+    create_techmdfile(testpath, 'mix', sample_data)
+
+    element_tree = lxml.etree.parse(
+        os.path.join(
+            testpath,
+            'mix-455752263d67f67402b0dc9e7119e5b3-othermd.xml'
+        )
+    )
+
+    # The file should contain one techmd element
+    techmd_elements = element_tree.xpath(
+        '/mets:mets/mets:amdSec/mets:techMD',
+        namespaces={"mets": "http://www.loc.gov/METS/"}
+    )
+    assert len(techmd_elements) == 1
+
+    # The techMD element should contain one sampleData element
+    sample_data_elements = techmd_elements[0].xpath('//sampleData')
+    assert len(sample_data_elements) == 1
