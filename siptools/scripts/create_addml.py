9da626af201e4ac615f8982bf1b5d4e30b1f8a0e
Juho Kuisma <juho.kuisma@csc.fi>
2018-09-14 11:00:47 +0000

    TPAS-91 PEP8 corrections

--- siptools/scripts/create_addml.py
+++ siptools/scripts/create_addml.py
@@ -2,16 +2,17 @@
 
 import os
 import argparse
-import siptools.utils
-import addml
 import hashlib
-import xml_helpers
 import lxml.etree as ET
+
+import addml
+import xml_helpers
+import siptools.utils
 
 
 def parse_arguments(arguments):
     """Parse arguments commandline arguments."""
-    
+
     parser = argparse.ArgumentParser(
         description="Tool for creating ADDML metadata for a CSV file. The "
                     "ADDML metadata is written to <hash>-ADDML-techmd.xml "
@@ -21,7 +22,7 @@ def parse_arguments(arguments):
                     "just the new CSV file name is appended to the existing "
                     "metadata."
     )
-    
+
     parser.add_argument('--path', type=str, required=True,
                         help="Path to the dir of CSV file")
     parser.add_argument('--fname', type=str, required=True,
@@ -49,14 +50,14 @@ def main(arguments=None):
     """Write ADDML metadata for a CSV file."""
     args = parse_arguments(arguments)
     create_addml_techmdfile(
-            args.path, args.fname, args.delim, args.header,
-            args.charset, args.sep, args.quot, args.work)
+        args.path, args.fname, args.delim, args.header,
+        args.charset, args.sep, args.quot, args.work)
 
 
 def create_addml_techmdfile(
-        sip_creation_path, filename, delimiter, 
-        isheader, charset, recordSeparator, 
-        quotingChar, workspace):
+        sip_creation_path, filename, delimiter,
+        isheader, charset, record_separator,
+        quoting_char, workspace):
 
     """Creates  ADDML metadata for a CSV file, and writes it into a METS XML
     file in workspace. Adds reference to techMD reference file used in
@@ -69,19 +70,19 @@ def create_addml_techmdfile(
     :delimiter: Delimiter used in the CSV file
     :isheader: True if CSV has a header else False
     :charset: Charset used in the CSV file
-    :recordSeparator: Char used for separating CSV file fields
-    :quotingChar: Quotation char used in the CSV file
+    :record_separator: Char used for separating CSV file fields
+    :quoting_char: Quotation char used in the CSV file
     :workspace: Output directory
 
     :returns: None
     """
-   
+
     csv_file = os.path.join(sip_creation_path, filename)
 
     # Create ADDML metadata
     addml_data = create_addml(
-            sip_creation_path, filename, delimiter,
-            isheader, charset, recordSeparator, quotingChar)
+        sip_creation_path, filename, delimiter,
+        isheader, charset, record_separator, quoting_char)
 
     digest = hashlib.md5(xml_helpers.utils.serialize(addml_data)).hexdigest()
     techmd_fname = siptools.utils.encode_path("%s-ADDML-techmd.xml" % digest)
@@ -92,9 +93,9 @@ def create_addml_techmdfile(
         workspace, addml_data, 'OTHER', "8.3", "ADDML")
 
     # Append flatFile element to the created METS XML file
-    new_line = flatFile_str(filename, "ref_001")
+    new_line = flat_file_str(filename, "ref_001")
     append_line(techmd_fname, "<addml:flatFiles>", new_line)
-    
+
     # Add reference from image file to techMD
     siptools.utils.add_techmdreference(workspace, techmd_id, csv_file)
 
@@ -111,21 +112,21 @@ def append_line(fname, xml_elem, new_line):
     """
 
     # Read all the lines into memory
-    with open(fname, 'r') as f:
-        lines = f.readlines()
+    with open(fname, 'r') as f_in:
+        lines = f_in.readlines()
 
     # Overwrite the file appending line_content
-    with open(fname, 'w') as f:
-        
+    with open(fname, 'w') as f_out:
+
         for line in lines:
-            f.write(line)
+            f_out.write(line)
 
             if line.strip() == xml_elem:
                 indent = len(line) - len(line.lstrip()) + 2
-                f.write(" " * indent + new_line)
-            
+                f_out.write(" " * indent + new_line)
 
-def flatFile_str(fname, def_ref):
+
+def flat_file_str(fname, def_ref):
     """Returns addml:flatFile xml element as a string,
     which can be appended to the xml file.
 
@@ -133,10 +134,10 @@ def flatFile_str(fname, def_ref):
     :def_ref: definitionReference of the flatFile element
     """
 
-    flatFile = '<addml:flatFile name="%s" definitionReference="%s"/>\n' % (
-            fname, def_ref)
+    flat_file = '<addml:flatFile name="%s" definitionReference="%s"/>\n' % (
+        fname, def_ref)
 
-    return flatFile
+    return flat_file
 
 
 def csv_header(csv_file_path, delimiter, isheader=False, headername='header'):
@@ -159,9 +160,9 @@ def csv_header(csv_file_path, delimiter, isheader=False, headername='header'):
 
 def create_addml(
         sip_creation_path, filename,
-        delimiter, isheader, charset, 
-        recordSeparator, quotingChar):
-    
+        delimiter, isheader, charset,
+        record_separator, quoting_char):
+
     """Creates ADDML metadata for a csv file
     without flatFile element, which is added
     by create_addml_techmdfile() function.
@@ -174,59 +175,59 @@ def create_addml(
     :delimiter: Delimiter used in the CSV file
     :isheader: True if CSV has a header else False
     :charset: Charset used in the CSV file
-    :recordSeparator: Char used for separating CSV file fields
-    :quotingChar: Quotation char used in the CSV file
-    
+    :record_separator: Char used for separating CSV file fields
+    :quoting_char: Quotation char used in the CSV file
+
     :returns: ADDML metadata XML element
     """
 
     header = csv_header(
-            os.path.join(sip_creation_path, filename), delimiter, isheader)
+        os.path.join(sip_creation_path, filename), delimiter, isheader)
 
     description = ET.Element(addml.addml_ns('description'))
     reference = ET.Element(addml.addml_ns('reference'))
 
     headers = header.split(delimiter)
-    fieldDefinitions = addml.wrapper_elems('fieldDefinitions')
+    field_definitions = addml.wrapper_elems('fieldDefinitions')
 
     for col in headers:
         elems = addml.definition_elems('fieldDefinition', col, 'String')
-        fieldDefinitions.append(elems)
+        field_definitions.append(elems)
 
-    recordDefinition = addml.definition_elems(
-            'recordDefinition', 'record',
-            'rdef001', [fieldDefinitions])
-    recordDefinitions = addml.wrapper_elems(
-            'recordDefinitions', [recordDefinition])
+    record_definition = addml.definition_elems(
+        'recordDefinition', 'record',
+        'rdef001', [field_definitions])
+    record_definitions = addml.wrapper_elems(
+        'recordDefinitions', [record_definition])
 
-    flatFileDefinition = addml.definition_elems(
-            'flatFileDefinition', 'ref001',
-            'rec001', [recordDefinitions])
-    flatFileDefinitions = addml.wrapper_elems(
-            'flatFileDefinitions', [flatFileDefinition])
+    flat_file_definition = addml.definition_elems(
+        'flatFileDefinition', 'ref001',
+        'rec001', [record_definitions])
+    flat_file_definitions = addml.wrapper_elems(
+        'flatFileDefinitions', [flat_file_definition])
 
-    dataType = addml.addml_basic_elem('dataType', 'string')
-    fieldType = addml.definition_elems(
-            'fieldType', 'String', child_elements=[dataType])
-    fieldTypes = addml.wrapper_elems('fieldTypes', [fieldType])
+    data_type = addml.addml_basic_elem('dataType', 'string')
+    field_type = addml.definition_elems(
+        'fieldType', 'String', child_elements=[data_type])
+    field_types = addml.wrapper_elems('fieldTypes', [field_type])
 
     trimmed = ET.Element(addml.addml_ns('trimmed'))
-    recordType = addml.definition_elems(
-            'recordType', 'rdef001',child_elements=[trimmed])
-    recordTypes = addml.wrapper_elems('recordTypes', [recordType])
+    record_type = addml.definition_elems(
+        'recordType', 'rdef001', child_elements=[trimmed])
+    record_types = addml.wrapper_elems('recordTypes', [record_type])
 
-    delimFileFormat = addml.delimfileformat(
-            recordSeparator, delimiter, quotingChar)
+    delim_file_format = addml.delimfileformat(
+        record_separator, delimiter, quoting_char)
     charset_elem = addml.addml_basic_elem('charset', charset)
-    flatFileType = addml.definition_elems(
-            'flatFileType', 'rec001',
-            child_elements=[charset_elem, delimFileFormat])
-    flatFileTypes = addml.wrapper_elems('flatFileTypes', [flatFileType])
+    flat_file_type = addml.definition_elems(
+        'flatFileType', 'rec001',
+        child_elements=[charset_elem, delim_file_format])
+    flat_file_types = addml.wrapper_elems('flatFileTypes', [flat_file_type])
 
-    structureTypes = addml.wrapper_elems(
-            'structureTypes', [flatFileTypes, recordTypes, fieldTypes])
+    structure_types = addml.wrapper_elems(
+        'structureTypes', [flat_file_types, record_types, field_types])
     flatfiles = addml.wrapper_elems(
-            'flatFiles', [flatFileDefinitions, structureTypes])
+        'flatFiles', [flat_file_definitions, structure_types])
 
     addml_root = addml.addml([description, reference, flatfiles])
 
--- tests/scripts/test_create_addml.py
+++ tests/scripts/test_create_addml.py
@@ -1,30 +1,31 @@
 """Tests for ``siptools.scripts.create_addml`` module"""
+
 import os.path
-import siptools.scripts.create_addml as create_addml
-import addml
 import lxml.etree as ET
 
+import siptools.scripts.create_addml as create_addml
 
-sip_creation_path = "tests/data/"
-csv_filename = "csvfile.csv"
-delimiter = ";"
-isheader = False
-charset = "UTF-8"
-recordSeparator = "CR+LF"
-quotingChar = '"'
 
-addml_ns = './/{http://www.arkivverket.no/standarder/addml}'
+SIP_CREATION_PATH = "tests/data/"
+CSV_FILENAME = "csvfile.csv"
+DELIMITER = ";"
+ISHEADER = False
+CHARSET = "UTF-8"
+RECORDSEPARATOR = "CR+LF"
+QUOTINGCHAR = '"'
+
+ADDML_NS = './/{http://www.arkivverket.no/standarder/addml}'
 
 def test_create_addml():
     """Test that ``create_addml`` returns valid addml."""
 
     addml_etree = create_addml.create_addml(
-            sip_creation_path, csv_filename, delimiter,
-            isheader, charset, recordSeparator, quotingChar)
+        SIP_CREATION_PATH, CSV_FILENAME, DELIMITER,
+        ISHEADER, CHARSET, RECORDSEPARATOR, QUOTINGCHAR)
 
     # Check namespace
     assert addml_etree.nsmap['addml'] == \
-            'http://www.arkivverket.no/standarder/addml'
+        'http://www.arkivverket.no/standarder/addml'
 
     # Check schema
     assert addml_etree.get(
@@ -32,47 +33,47 @@ def test_create_addml():
     ) == "http://www.arkivverket.no/standarder/addml "\
          "http://schema.arkivverket.no/ADDML/latest/addml.xsd"
 
-    # Check individual elements    
+    # Check individual elements
     tags = ["fieldSeparatingChar", "charset", "recordSeparator", "quotingChar"]
     vals = [";", "UTF-8", "CR+LF", '"']
 
-    for i in range(len(tags)):
-        val = addml_etree.find(addml_ns + tags[i]).text
+    for i, tag in enumerate(tags):
+        val = addml_etree.find(ADDML_NS + tag).text
         assert val == vals[i]
 
     # Check that flatFile element is not added
-    assert addml_etree.find(addml_ns + "flatFile") is None
+    assert addml_etree.find(ADDML_NS + "flatFile") is None
 
     # Check that correct amount of headers is created
-    assert len(addml_etree.find(addml_ns + "fieldDefinitions")) == 3
-    
+    assert len(addml_etree.find(ADDML_NS + "fieldDefinitions")) == 3
+
 
 def test_create_addml_techmdfile(testpath):
     """
     Test that ``create_addml_techmdfile`` writes addml file and techMD
     reference file without unnecessary duplication.
     """
-    
-    # Call create_addml_techmdfile() twice with 
+
+    # Call create_addml_techmdfile() twice with
     # same metadata, but different filename
     create_addml.create_addml_techmdfile(
-            sip_creation_path, "simple_csv.csv", ',', isheader,
-            charset, recordSeparator, quotingChar, testpath)
+        SIP_CREATION_PATH, "simple_csv.csv", ',', ISHEADER,
+        CHARSET, RECORDSEPARATOR, QUOTINGCHAR, testpath)
 
     create_addml.create_addml_techmdfile(
-            sip_creation_path, "simple_csv_2.csv", ',', isheader,
-            charset, recordSeparator, quotingChar, testpath)
+        SIP_CREATION_PATH, "simple_csv_2.csv", ',', ISHEADER,
+        CHARSET, RECORDSEPARATOR, QUOTINGCHAR, testpath)
 
     # Call create_addml_techmdfile() with different metadata
     create_addml.create_addml_techmdfile(
-            sip_creation_path, csv_filename, delimiter, isheader,
-            charset, recordSeparator, quotingChar, testpath)
+        SIP_CREATION_PATH, CSV_FILENAME, DELIMITER, ISHEADER,
+        CHARSET, RECORDSEPARATOR, QUOTINGCHAR, testpath)
 
     file1 = os.path.join(
-            testpath, '710ad5d65bbb4339fe265820f9ddbf8f-ADDML-techmd.xml')
-    
+        testpath, '710ad5d65bbb4339fe265820f9ddbf8f-ADDML-techmd.xml')
+
     file2 = os.path.join(
-            testpath, '4368cf94e94b8217f2210b0071dc5175-ADDML-techmd.xml')
+        testpath, '4368cf94e94b8217f2210b0071dc5175-ADDML-techmd.xml')
 
     # Check that techmdreference and the two ADDML-techmd files are created
     assert os.path.isfile(os.path.join(testpath, 'techmd-references.xml'))
@@ -82,16 +83,16 @@ def test_create_addml_techmdfile(testpath):
     # Parse ADDML-techmd files to check that right flatFiles are added
     root1 = ET.parse(file1)
     root2 = ET.parse(file2)
-    
-    flatFiles1 = root1.find(addml_ns + "flatFiles")    
-    flatFiles2 = root2.find(addml_ns + "flatFiles")
+
+    flat_files1 = root1.find(ADDML_NS + "flatFiles")
+    flat_files2 = root2.find(ADDML_NS + "flatFiles")
 
     # Check number of child elements
-    assert len(flatFiles1) == 4
-    assert len(flatFiles2) == 3
+    assert len(flat_files1) == 4
+    assert len(flat_files2) == 3
 
     # Check flatFile name attributes
-    assert flatFiles1[0].get("name") == "simple_csv_2.csv"
-    assert flatFiles1[1].get("name") == "simple_csv.csv"
+    assert flat_files1[0].get("name") == "simple_csv_2.csv"
+    assert flat_files1[1].get("name") == "simple_csv.csv"
 
-    assert flatFiles2[0].get("name") == "csvfile.csv"
+    assert flat_files2[0].get("name") == "csvfile.csv"
