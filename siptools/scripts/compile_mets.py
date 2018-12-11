2e0dba553d1c9fad23e3458aa05e0f9dbaefc518
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-12-11 18:55:54 +0200

    Add public function for compiling METS

--- siptools/scripts/compile_mets.py
+++ siptools/scripts/compile_mets.py
@@ -16,8 +16,7 @@ from siptools.utils import decode_path
 
 
 def parse_arguments(arguments):
-    """Parse arguments
-    """
+    """Parse arguments"""
     parser = argparse.ArgumentParser(description="Tool for creating mets")
     parser.add_argument(
         'mets_profile', type=str, choices=METS_PROFILE,
@@ -68,40 +67,22 @@ def main(arguments=None):
     """
     args = parse_arguments(arguments)
 
-    # Create mets header
-    _mets = mets.mets(METS_PROFILE[args.mets_profile], objid=args.objid,
-                      label=args.label, namespaces=NAMESPACES)
-    _mets = mets_extend(_mets, METS_CATALOG, METS_SPECIFICATION,
-                        args.contentid, args.contractid)
-    # Create list of additional agent elements if packagingservice is defined
-    if args.packagingservice:
-        _agents = [mets.agent(args.organization_name,
-                              agent_role='ARCHIVIST')]
-        _agents.append(mets.agent(args.packagingservice, agent_type='OTHER',
-                                  agent_role='CREATOR', othertype='SOFTWARE'))
-    else:
-        _agents = [mets.agent(args.organization_name)]
-    _metshdr = mets.metshdr(args.create_date, args.last_moddate,
-                            args.record_status, _agents)
-    _mets.append(_metshdr)
-
-    # Collect elements from workspace XML files
-    elements = []
-    for entry in scandir(args.workspace):
-        if entry.name.endswith(('-techmd.xml', '-agent.xml', '-event.xml',
-                                'dmdsec.xml', 'structmap.xml', 'filesec.xml',
-                                'rightsmd.xml')) and entry.is_file():
-            element = lxml.etree.parse(entry.path).getroot()[0]
-            elements.append(element)
-
-    elements = mets.merge_elements('{%s}amdSec' % NAMESPACES['mets'], elements)
-    elements.sort(key=mets.order)
-
-    for element in elements:
-        _mets.append(element)
+    mets_document = create_mets(
+        args.workspace,
+        mets_attributes={'PROFILE': args.mets_profile,
+                         'OBJID': args.objid,
+                         'LABEL': args.label,
+                         "CONTENTID": args.contentid,
+                         "CONTRACTID": args.contractid},
+        metshdr_attributes={"CREATEDATE": args.create_date,
+                            "LASTMODDATE": args.last_moddate,
+                            "RECORDSTATUS": args.record_status},
+        organization=args.organization_name,
+        packagingservice=args.packagingservice
+    )
 
     if args.stdout:
-        print h.serialize(_mets)
+        print h.serialize(mets_document.getroot())
 
     output_file = os.path.join(args.workspace, 'mets.xml')
 
@@ -109,7 +90,7 @@ def main(arguments=None):
         os.makedirs(os.path.dirname(output_file))
 
     with open(output_file, 'w+') as outfile:
-        outfile.write(h.serialize(_mets))
+        outfile.write(h.serialize(mets_document.getroot()))
 
     print "compile_mets created file: %s" % output_file
 
@@ -123,6 +104,73 @@ def main(arguments=None):
         print "compile_mets cleaned work files from workspace"
 
     return 0
+
+
+def create_mets(workspace, mets_attributes, metshdr_attributes,
+                organization, packagingservice=None):
+    """Creates METS document element tree. Looks for files with prefix
+    "-techmd.xml", "-agent.xml", "-event.xml", "dmdsec.xml", "structmap.xml",
+    "filesec.xml", and "rightsmd.xml" from workspace and merges the dmdSec,
+    amdSec, fileSec, and structMap elements (one element from each file) into
+    one METS document. Also metsHdr element is created and included in
+    document.
+
+    :param workspace: path to directory where files are searched
+    :param dict mets_attributes: attributes of mets element: "PROFILE",
+                                 "OBJID", "LABEL", "CONTENTID", and
+                                 "CONTRACTID"
+    :param dict metshdr_attributes: attributes of metsHdr element:
+                                    "CREATEDATE", "LASTMODDATE" and
+                                    "RECORDSTATUS"
+    :param organization: name of CREATOR agent
+    :param packagingservice: Add ``packagingservice`` as CREATOR agent.
+                             ``organization`` is used as ARCHIVIST agent.
+    :returns: METS document ElementTree object
+    """
+    # Create list of agent elements
+    if packagingservice:
+        agents = [mets.agent(organization, agent_role='ARCHIVIST')]
+        agents.append(mets.agent(packagingservice,
+                                 agent_type='OTHER',
+                                 agent_role='CREATOR',
+                                 othertype='SOFTWARE'))
+    else:
+        agents = [mets.agent(organization, agent_role='CREATOR')]
+
+    # Create mets header
+    metshdr = mets.metshdr(metshdr_attributes["CREATEDATE"],
+                           metshdr_attributes["LASTMODDATE"],
+                           metshdr_attributes["RECORDSTATUS"],
+                           agents)
+
+    # Collect elements from workspace XML files
+    elements = []
+    for entry in scandir(workspace):
+        if entry.name.endswith(('-techmd.xml', '-agent.xml', '-event.xml',
+                                'dmdsec.xml', 'structmap.xml', 'filesec.xml',
+                                'rightsmd.xml')) and entry.is_file():
+            element = lxml.etree.parse(entry.path).getroot()[0]
+            elements.append(element)
+
+    elements = mets.merge_elements('{%s}amdSec' % NAMESPACES['mets'], elements)
+    elements.sort(key=mets.order)
+
+    # Create METS element
+    mets_element = mets.mets(METS_PROFILE[mets_attributes["PROFILE"]],
+                             objid=mets_attributes["OBJID"],
+                             label=mets_attributes["LABEL"],
+                             namespaces=NAMESPACES)
+    mets_element = mets_extend(mets_element,
+                               METS_CATALOG,
+                               METS_SPECIFICATION,
+                               mets_attributes["CONTENTID"],
+                               mets_attributes["CONTRACTID"])
+    mets_element.append(metshdr)
+    for element in elements:
+        mets_element.append(element)
+    lxml.etree.cleanup_namespaces(mets_element)
+
+    return lxml.etree.ElementTree(mets_element)
 
 
 def clean_metsparts(path):
