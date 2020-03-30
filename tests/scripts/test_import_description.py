b0693b245a04ff86467e1a4b9487b9a45a915534
Johan Kylander <johan.kylander@csc.fi>
2020-03-30 16:28:02 +0300

    SAPA-382 Add unit tests for event and agent creation in import-description

--- tests/scripts/test_import_description.py
+++ tests/scripts/test_import_description.py
@@ -10,21 +10,27 @@ import lxml.etree as ET
 from siptools.scripts import import_description
 from siptools.scripts.import_description import main
 from siptools.utils import fsdecode_path
+from siptools.xml.mets import NAMESPACES
 
 
-def get_md_file(path, input_target):
+def get_md_file(path,
+                input_target,
+                ref_file='import-description-md-references.xml',
+                output_suffix='-dmdsec.xml'):
     """Get id"""
-    ref = os.path.join(path, 'import-description-md-references.xml')
+    root = ET.parse(os.path.join(path, ref_file)).getroot()
+    id_xpath = "/mdReferences/mdReference[@directory='%s']" % \
+        fsdecode_path(input_target)
 
-    root = ET.parse(ref).getroot()
-    amdref = root.xpath("/mdReferences/mdReference"
-                        "[@directory='%s']" % fsdecode_path(input_target))[0]
-    output = os.path.join(path, amdref.text[1:] +
-                          "-dmdsec.xml")
-    return output
+    for amdref in root.xpath(id_xpath):
+        output = os.path.join(path, amdref.text[1:] +
+                              output_suffix)
+        if os.path.exists(output):
+            return output
+    return None
 
 
-#pylint: disable=invalid-name
+# pylint: disable=invalid-name
 def test_import_description_valid_file(testpath, run_cli):
     """ Test case for single valid xml-file"""
     dmdsec_location = 'tests/data/import_description/metadata/' \
@@ -42,6 +48,17 @@ def test_import_description_valid_file(testpath, run_cli):
     assert len(root.xpath('./*/*/*/*')) == 4
     assert root.xpath('./*/*/*/*')[0].tag == \
         '{http://purl.org/dc/elements/1.1/}title'
+
+    # Assert that event has been created
+    event_output = get_md_file(
+        testpath,
+        dmdsec_target,
+        ref_file='premis-event-md-references.xml',
+        output_suffix='-PREMIS%3AEVENT-amd.xml')
+    event_output_path = os.path.join(testpath, event_output)
+    event_root = ET.parse(event_output_path).getroot()
+    assert event_root.xpath('./*/*/*/*/*')[0].tag == \
+        '{info:lc/xmlns/premis-v2}event'
 
 
 @pytest.mark.parametrize(
@@ -71,7 +88,7 @@ def test_invalid_dmd_target_path():
         import_description.dmd_target_path('.', 'foo/bar')
 
 
-#pylint: disable=invalid-name
+# pylint: disable=invalid-name
 def test_import_description_file_not_found(testpath, run_cli):
     """ Test case for not existing xml-file."""
     dmdsec_location = 'tests/data/import_description/metadata/' \
@@ -97,7 +114,7 @@ def test_import_description_no_xml(testpath, run_cli):
     assert isinstance(result.exception, ET.XMLSyntaxError)
 
 
-#pylint: disable=invalid-name
+# pylint: disable=invalid-name
 def test_import_description_invalid_namespace(testpath, run_cli):
     """ test case for invalid namespace in XML file """
     dmdsec_location = 'tests/data/import_description/dc_invalid_ns.xml'
@@ -153,3 +170,49 @@ def test_paths(testpath, directory, base_path, run_cli):
 
     assert 'directory=\"%s\"' % os.path.normpath(directory) in md_references
     assert os.path.isdir(os.path.normpath(os.path.join(base_path, directory)))
+
+
+def test_import_description_event_agent(testpath, run_cli):
+    """Test that the script import_description creates an event and agent with
+    the proper metadata.
+    """
+    dmdsec_location = 'tests/data/import_description/metadata/' \
+        'dc_description.xml'
+    dmdsec_target = 'tests/data/structured'
+
+    run_cli(main, [
+        dmdsec_location, '--dmdsec_target', dmdsec_target,
+        '--workspace', testpath, '--remove_root', '--dmd_source', 'database',
+        '--dmd_agent', 'database-client', 'software'])
+
+    event_output = get_md_file(
+        testpath,
+        dmdsec_target,
+        ref_file='premis-event-md-references.xml',
+        output_suffix='-PREMIS%3AEVENT-amd.xml')
+    event_output_path = os.path.join(testpath, event_output)
+    event_root = ET.parse(event_output_path).getroot()
+    assert event_root.xpath('./*/*/*/*/*')[0].tag == \
+        '{info:lc/xmlns/premis-v2}event'
+    assert event_root.xpath(
+        './/premis:eventType',
+        namespaces=NAMESPACES)[0].text == 'metadata extraction'
+    assert event_root.xpath(
+        './/premis:eventOutcomeDetailNote',
+        namespaces=NAMESPACES)[0].text == ('Descriptive metadata imported '
+                                           'to mets dmdSec from database')
+    agent_output = get_md_file(
+        testpath,
+        dmdsec_target,
+        ref_file='premis-event-md-references.xml',
+        output_suffix='-PREMIS%3AAGENT-amd.xml')
+    agent_output_path = os.path.join(testpath, agent_output)
+    agent_root = ET.parse(agent_output_path).getroot()
+    assert agent_root.xpath('./*/*/*/*/*')[0].tag == \
+        '{info:lc/xmlns/premis-v2}agent'
+    assert agent_root.xpath(
+        './/premis:agentName',
+        namespaces=NAMESPACES)[0].text == 'database-client'
+    assert agent_root.xpath(
+        './/premis:agentType',
+        namespaces=NAMESPACES)[0].text == 'software'
