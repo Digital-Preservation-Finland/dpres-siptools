e9e40f69efbc6ef894ff39d06ddca37681697de4
Vili Oja <vili.oja@csc.fi>
2020-07-28 11:32:02 +0300

    KDKPAS-2389 Edit test string checking

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -123,11 +123,12 @@ def test_filescraper_error():
                           skip_well_check=True)
 
     message = None
+    print(error.value.args)
     for arg in error.value.args:
         is_string = isinstance(arg, basestring) if six.PY2 \
             else isinstance(arg, str)
         if is_string:
-            if "Metadata" in arg[0:8]:
+            if "Metadata" in arg[0:14]:
                 message = utils.ensure_str(arg)
 
     assert filename in message
