77ef46fcc6537697af176dae170bdd73f84e98ee
Vili Oja <vili.oja@csc.fi>
2020-07-28 12:11:55 +0300

    KDKPAS-2389 Try exception without pytest

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -118,13 +118,22 @@ def test_filescraper_error():
 
     filename = utils.ensure_str("invalid_empty_text-file-åäö.txt")
 
-    with pytest.raises(ValueError) as error:
+#    with pytest.raises(ValueError) as error:
+#        utils.scrape_file("tests/data/invalid_empty_text-file-åäö.txt",
+#                          skip_well_check=True)
+
+    error = None
+    try:
         utils.scrape_file("tests/data/invalid_empty_text-file-åäö.txt",
                           skip_well_check=True)
+    except ValueError as e:
+        error = e
+
+    assert error is not None
 
     message = None
-    print(error.value.args)
-    for arg in error.value.args:
+    print(error.args)
+    for arg in error.args:
         is_string = isinstance(arg, basestring) if six.PY2 \
             else isinstance(arg, str)
         if is_string:
