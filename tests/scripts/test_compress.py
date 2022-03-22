ba89239586321ff5053149ddb79ca9ebd20e5d51
Juha Lehtonen <juha.lehtonen@csc.fi>
2022-03-22 12:51:12 +0200

    PASMM-59 Add tar test to have several files to be excluded in the root dir of the source.

--- tests/scripts/test_compress.py
+++ tests/scripts/test_compress.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import os
 import tarfile
 import subprocess
+import pytest
 import siptools.scripts.compress
 
 
@@ -26,29 +27,35 @@ def test_compress(testpath, run_cli):
     assert child.returncode == 0
 
 
-def test_exclude(testpath, run_cli):
+@pytest.mark.parametrize(("directory", "ending"), [
+    ("structured", ".txt"),
+    ("images", ".tif")
+])
+def test_exclude(testpath, run_cli, directory, ending):
     """
-    Test excluding files.
+    Test excluding files. In test case 1 we have several txt files in various
+    subdirectories. In test case 2 we have several tiff files in a single
+    directory.
     """
     dir_to_tar = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '..', 'data', 'structured'))
+        os.path.join(os.path.dirname(__file__), '..', 'data', directory))
     output = os.path.join(testpath, 'sip.tar')
 
     # click.CliRunner interprets a single string argument as Unix command.
     # This is now needed (instead of an argument list) to test single quotes.
-    arguments = "%s --tar_filename %s --exclude '*.txt'" % (
-        dir_to_tar, output)
+    arguments = "%s --tar_filename %s --exclude '*%s'" % (
+        dir_to_tar, output, ending)
 
     run_cli(siptools.scripts.compress.main, arguments)
 
     count = 0
     for _, _, files in os.walk(dir_to_tar):
         for name in files:
-            if name.endswith(".txt"):
+            if name.endswith(ending):
                 count += 1
-    assert count > 1  # Make sure that we have multiple txt files
+    assert count > 1  # Make sure that we have multiple files to exclude
 
-    # No txt files present in TAR
+    # No excluded files present in TAR
     with tarfile.open(output) as tar:
         for name in tar.getnames():
-            assert not name.endswith(".txt")
+            assert not name.endswith(ending)
