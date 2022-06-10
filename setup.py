58c723f5db6a035cca6f9c093e1f0c4ae02720dc
Juha Lehtonen <juha.lehtonen@csc.fi>
2022-06-10 19:07:48 +0300

    KDKPAS-2852 Fix requirements in setup files.

--- setup.py
+++ setup.py
@@ -32,35 +32,26 @@ def main():
         install_requires=[
             "lxml",
             "scandir ; python_version == '2.7'",
-            "file-magic",
-            "pymediainfo",
-            "wand>=0.5.1",
-            "Pillow",
             "six",
-            "olefile",
-            "opf-fido==1.4.0",
             "click",
-            "ffmpeg-python",
-            "M2Crypto",
-            "python-mimeparse",
-            'xml_helpers@git+https://gitlab.ci.csc.fi/dpres/'
-            'xml-helpers.git@develop#egg=xml_helpers',
-            'mets@git+https://gitlab.ci.csc.fi/dpres/mets.git'
-            '@develop#egg=mets',
-            'premis@git+https://gitlab.ci.csc.fi/dpres/premis.git'
-            '@develop#egg=premis',
-            'dpres_signature@git+https://gitlab.ci.csc.fi/dpres/'
-            'dpres-signature.git@develop#egg=dpres_signature',
-            'nisomix@git+https://gitlab.ci.csc.fi/dpres/nisomix.git'
-            '@develop#egg=nisomix',
-            'addml@git+https://gitlab.ci.csc.fi/dpres/addml.git'
-            '@develop#egg=addml',
-            'audiomd@git+https://gitlab.ci.csc.fi/dpres/audiomd.git'
-            '@develop#egg=audiomd',
-            'videomd@git+https://gitlab.ci.csc.fi/dpres/videomd.git'
-            '@develop#egg=videomd',
-            'file_scraper@git+https://gitlab.ci.csc.fi/dpres/'
-            'file-scraper.git@develop#egg=file_scraper'
+            "xml_helpers@git+https://gitlab.ci.csc.fi/dpres/"
+            "xml-helpers.git@develop#egg=xml_helpers",
+            "mets@git+https://gitlab.ci.csc.fi/dpres/mets.git"
+            "@develop#egg=mets",
+            "premis@git+https://gitlab.ci.csc.fi/dpres/premis.git"
+            "@develop#egg=premis",
+            "dpres_signature@git+https://gitlab.ci.csc.fi/dpres/"
+            "dpres-signature.git@develop#egg=dpres_signature",
+            "nisomix@git+https://gitlab.ci.csc.fi/dpres/nisomix.git"
+            "@develop#egg=nisomix",
+            "addml@git+https://gitlab.ci.csc.fi/dpres/addml.git"
+            "@develop#egg=addml",
+            "audiomd@git+https://gitlab.ci.csc.fi/dpres/audiomd.git"
+            "@develop#egg=audiomd",
+            "videomd@git+https://gitlab.ci.csc.fi/dpres/videomd.git"
+            "@develop#egg=videomd",
+            "file_scraper@git+https://gitlab.ci.csc.fi/dpres/"
+            "file-scraper.git@develop#egg=file_scraper"
         ],
         entry_points={'console_scripts': scripts_list()}
     )
